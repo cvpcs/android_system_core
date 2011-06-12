da6efb2add24d930dcfab9f48681c9a610aa01f1
Austen Dicken <cvpcsm@gmail.com>
2011-06-11 20:00:37 -0500

    build to logwrapper.bin if we have enabled hijacking
    Change-Id: Idb0cd39db7e158d807dd1b54b17ba0c1f3f7d7e1

--- logwrapper/Android.mk
+++ logwrapper/Android.mk
@@ -2,6 +2,13 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= logwrapper.c
+
+ifneq ($(BOARD_HIJACK_ENABLE),true)
 LOCAL_MODULE := logwrapper
+else
+LOCAL_MODULE := logwrapper.bin
+LOCAL_MODULE_TAGS := eng
+endif
+
 LOCAL_STATIC_LIBRARIES := liblog
 include $(BUILD_EXECUTABLE)
